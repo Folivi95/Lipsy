1c060cec60119fe242a632e21f2bca3091ca8609
joshuafolivi <joshua.folivi@yahoo.com>
2020-01-18 12:46:16 +0100

    added order controller, order repo and check out page

---
+++ Lipsy/Controllers/OrderController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Lipsy.Data.Interfaces;
+using Lipsy.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Lipsy.Controllers
+{
+    public class OrderController : Controller
+    {
+        private readonly IOrderRepository orderRepository;
+        private readonly ShoppingCart shoppingCart;
+
+        public OrderController(IOrderRepository orderRepository, ShoppingCart shoppingCart)
+        {
+            this.orderRepository = orderRepository;
+            this.shoppingCart = shoppingCart;
+        }
+
+        public IActionResult CheckOut()
+        {
+            return View();
+        }
+    }
+}
---
+++ Lipsy/Data/Repositories/OrderRepository.cs
@@ -0,0 +1,43 @@
+﻿using Lipsy.Data.Interfaces;
+using Lipsy.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Lipsy.Data.Repositories
+{
+    public class OrderRepository : IOrderRepository
+    {
+        private readonly AppDbContext appDbContext;
+        private readonly ShoppingCart shoppingCart;
+
+        public OrderRepository(AppDbContext appDbContext, ShoppingCart shoppingCart)
+        {
+            this.appDbContext = appDbContext;
+            this.shoppingCart = shoppingCart;
+        }
+
+        public void CreateOrder(Order order)
+        {
+            order.OrderPlaced = DateTime.Now;
+            this.appDbContext.Orders.Add(order);
+
+            var shoppingCartItems = this.shoppingCart.ShoppingCartItems;
+
+            foreach (var item in shoppingCartItems)
+            {
+                var orderDetail = new OrderDetail()
+                {
+                    Amount = item.Amount,
+                    LipstickId = item.Lipstick.LipstickId,
+                    OrderId = order.OrderId,
+                    Price = item.Lipstick.Price
+                };
+                this.appDbContext.OrderDetails.Add(orderDetail);
+            }
+
+            this.appDbContext.SaveChanges();
+        }
+    }
+}
